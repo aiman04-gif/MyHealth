2ff44c6ba76fbf1ededb324a6da27c882c5d7780
aiman04-gif <125193229+aiman04-gif@users.noreply.github.com>
2026-05-01 19:39:01 +0500

    Login Signup using Firebase Auth Complete

--- app/src/main/java/com/example/myhealth/Login.java
+++ app/src/main/java/com/example/myhealth/Login.java
@@ -1,14 +1,30 @@
 package com.example.myhealth;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Login extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private EditText etEmail, etPassword;
+    private TextView tv_signup;
+    private View btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +36,47 @@ public class Login extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        init();
+        auth= FirebaseAuth.getInstance();
+
+        btn_login.setOnClickListener(v->{
+            String email=etEmail.getText().toString().trim();
+            String password=etPassword.getText().toString().trim();
+
+            if(email.isEmpty()){
+                etEmail.setError("Enter Email");
+            }else if(password.isEmpty())
+            {
+                etPassword.setError("Enter Password");
+            }else{
+                auth.signInWithEmailAndPassword(email,password)
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(Login.this, "Login Unsuccessful"+ e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+
+
+        tv_signup.setOnClickListener(v->{
+            Intent i = new Intent(Login.this, SignUp.class);
+            startActivity(i);
+        });
+    }
+
+    private void init()
+    {
+        tv_signup= findViewById(R.id.tv_signup);
+        etEmail=findViewById(R.id.etEmail);
+        etPassword=findViewById(R.id.etPassword);
+        btn_login= findViewById(R.id.btn_login);
     }
 }
---
+++ app/src/main/java/com/example/myhealth/SignUp.java
@@ -0,0 +1,96 @@
+package com.example.myhealth;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUp extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private EditText etFullName, etEmail, etPassword, etCPassword;
+    private View btn_signUp;
+    private TextView tv_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sign_up);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        init();
+        auth= FirebaseAuth.getInstance();
+
+        tv_login.setOnClickListener(v->{
+            Intent i = new Intent(SignUp.this, Login.class);
+            startActivity(i);
+            finish();
+        });
+
+        btn_signUp.setOnClickListener(v->{
+            String name=etFullName.getText().toString().trim();
+            String email=etEmail.getText().toString().trim();
+            String password= etPassword.getText().toString().trim();
+            String cpassword= etCPassword.getText().toString().trim();
+
+            if(name.isEmpty())
+            {
+                etFullName.setError("Name can not be empty");
+            }else if(email.isEmpty())
+            {
+                etEmail.setError("Email can not be empty");
+            }else if(password.isEmpty())
+            {
+                etPassword.setError("Password can not be empty");
+            }else if(cpassword.isEmpty())
+            {
+                etCPassword.setError("Password can not be empty");
+            }else if(!password.equals(cpassword)){
+                Toast.makeText(this,"Password and Confirm Password should be same",Toast.LENGTH_SHORT).show();
+            }else
+            {
+                auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(SignUp.this, "Signup Successful", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(SignUp.this, Login.class));
+                        }else{
+                            Toast.makeText(SignUp.this, "Sign Up unsuccessful"+task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+
+        });
+    }
+
+    private void init()
+    {
+        tv_login= findViewById(R.id.tv_login);
+        etFullName=findViewById(R.id.etFullName);
+        etEmail=findViewById(R.id.etEmail);
+        etPassword=findViewById(R.id.etPassword);
+        etCPassword=findViewById(R.id.etCPassword);
+        btn_signUp=findViewById(R.id.btn_signup);
+    }
+}
